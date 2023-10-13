aa8b14143bc3493a8b251fcbe9bd6d0a0eafbc81
rofinafiin <rofinafiisr@gmail.com>
2023-10-13 06:43:04 +0100

    ganti return

--- geojson.go
+++ geojson.go
@@ -2,11 +2,11 @@ package gcfbackend
 
 import "encoding/json"
 
-func GCHandlerFunc(Mongostring, dbname, colname string) string {
+func GCHandlerFunc(Mongostring, dbname, colname string) []byte {
 	koneksyen := GetConnectionMongo(Mongostring, dbname)
 	datageo := GetAllGeoData(koneksyen, colname)
 
 	jsoncihuy, _ := json.Marshal(datageo)
 
-	return string(jsoncihuy)
+	return jsoncihuy
 }
